fixed some problems with restarting continuum calculation not working correctly

spectral_data_source_helper/calc/numba/spec.py
@@ -421,7 +421,8 @@ def line_strength_at_temp(
 
 
 
-@njit(parallel=spectral_data_source_helper.calc.numba.PARALLEL)
+#@njit(parallel=spectral_data_source_helper.calc.numba.PARALLEL)
+@njit(parallel=False)
 def accumulate_pseudocontinuum_1d(
 	weak_line_mask : np.ndarray, #[N_lines]
 	bin_indices : np.ndarray, #[N_lines]
@@ -429,18 +430,20 @@ def accumulate_pseudocontinuum_1d(
 	line_strength_sum : np.ndarray, #[N_bins]
 	input_block : np.ndarray, #[N_lines, N_pairs]
 	output_block : np.ndarray, #[N_bins, N_pairs]
+	n_lines : int,
+	n_bins : int,
+	n_pairs : int,
 ):
-	n_pairs = input_block.shape[1]
 
 	# Reset accumulators
-	for x in range(line_strength_sum.shape[0]):
+	for x in range(n_bins):
 		line_strength_sum[x] = 0
 		for k in range(n_pairs):
 			output_block[x][k] = 0
 
 	# NOTE: parallel loop may have race condition on accumulators, NUMBA says it
 	# accounts for that but this should be checked
-	for i in prange(weak_line_mask.shape[0]):
+	for i in range(n_lines):
 		if not weak_line_mask[i]:
 			continue
 		
@@ -452,7 +455,7 @@ def accumulate_pseudocontinuum_1d(
 			output_block[x][k] += line_strengths_at_temp[i] * input_block[i][k]
 
 
-@njit(parallel=False)
+@njit(parallel=spectral_data_source_helper.calc.numba.PARALLEL)
 def accumulate_pseudocontinuum(
 	weak_line_mask : np.ndarray, #[N_temp, N_lines]
 	bin_indices : np.ndarray, #[N_temp, N_lines]
@@ -461,7 +464,11 @@ def accumulate_pseudocontinuum(
 	input_block : np.ndarray, #[N_lines, N_pairs]
 	output_block : np.ndarray, #[N_temp, N_bins, N_pairs]
 ):
-	for j in range(weak_line_mask.shape[0]):
+	n_temp = weak_line_mask.shape[0]
+	n_lines = weak_line_mask.shape[1]
+	n_bins = output_block.shape[1]
+	n_pairs = output_block.shape[2]
+	for j in prange(n_temp):
 		accumulate_pseudocontinuum_1d(
 			weak_line_mask[j],
 			bin_indices[j],
@@ -469,4 +476,7 @@ def accumulate_pseudocontinuum(
 			line_strength_sum[j],
 			input_block,
 			output_block[j],
-		)	
+			n_lines,
+			n_bins,
+			n_pairs,
+		)

spectral_data_source_helper/exomol/__main__.py
@@ -288,6 +288,7 @@ def action_calc_continuum(
 		continuum_n_bins : int = 1_000,
 		continuum_bin_spacing : Literal['lin', 'log'] = 'lin',
 		chunk_size : int = CHUNK_SIZE,
+		force_restart : bool = False,
 ):
 	#pkg_logger.setLevel(logging.WARN) # SET LOGGING SO WE HAVE CLEAR OUTPUT
 	progress_lgr.setLevel(logging.INFO) # SET LOGGING SO WE HAVE CLEAR OUTPUT
@@ -311,21 +312,72 @@ def action_calc_continuum(
 		total_weak_lines_in_continuum = np.zeros(temperature_arr.shape, dtype=int)
 	
 	
-		contbins_fpaths, continuum_fpaths, stronglines_fpaths = (tuple(REPO_LOCAL / fname for fname in fnames) for fnames in ds_holder.get_line_and_continuum_fnames_at_temp(temperature_arr))
-	
+		progress_fpath, contbins_fpaths, continuum_fpaths, stronglines_fpaths = ds_holder.get_line_and_continuum_fpaths_at_temp(temperature_arr, dir=REPO_LOCAL)
+		
 		dt_start = dt.datetime.now()
 		dt_split_2 = dt_start
-	
+		
+		progress_header = (
+			'\n'.join(('\n'.join(map(lambda x: str(x.relative_to(progress_fpath.parent)), fpaths)) for fpaths in zip(stronglines_fpaths, continuum_fpaths, contbins_fpaths)))
+			+ '\n' 
+			+ ('0 '*(1+2*temperature_arr.size)) # <total_lines_processed> <total_strong_lines_array> <total_weak_lines_array>
+			+'\n'
+		)
+		
+		# Check if we are continuing an interrupted calculation
+		n_lines_previously_processed = 0
+		if not force_restart and progress_fpath.exists():
+			with open(progress_fpath, 'r') as progress_fhdl:
+				progress_contents = progress_fhdl.read()
+				if progress_header == progress_contents[:len(progress_header)]:
+					# we are operating upon the same set of files
+					progress_contents = progress_contents.rstrip()
+					
+					# Last line has
+					# <total_lines_processed> <total_strong_lines_array> <total_weak_lines_array>
+					total_lines_processed, *total_strong_weak_lines = map(int, progress_contents.rsplit('\n', maxsplit=1)[1].strip().split())
+					n_lines_previously_processed = total_lines_processed
+					total_strong_lines[:] = total_strong_weak_lines[:temperature_arr.size]
+					total_weak_lines_in_continuum[:] = total_strong_weak_lines[temperature_arr.size:]
+		
+		if n_lines_previously_processed == 0:
+			print(f'Starting calculation, initialising progress file "{progress_fpath.name}"')
+			with open(progress_fpath, 'w') as progress_fhdl:
+				progress_fhdl.write(progress_header)
+		else:
+			print(f'Continuuing interrupted calculation. Processed {n_lines_previously_processed} lines before interruption, will skip forward to that point.')
+			for i, fpath in enumerate(continuum_fpaths):
+				with spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'rb') as f:
+					pseudo_continuums[i] = f.read()
+			
+			
+			
+		progress_record_start_pos = len(progress_header)
+		del progress_header # this can be quite large so free up the memory
+		
+		
+		
 		# Write the continuum bins to their files
 		for contbins_fpath in contbins_fpaths:
 			with open(contbins_fpath, 'wb') as f:
 				continuum_bin_edges.tofile(f)
 			print(f'Written continum bin edge data to "{str(contbins_fpath)}"')
 		
+		stop_loop = False
+		continuum_fhdls = []
+		stronglines_fhdls = []
 		try:
-			continuum_fhdls = tuple(spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'wb') for fpath in continuum_fpaths)
-			stronglines_fhdls = tuple(spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'wb') for fpath in stronglines_fpaths)
-		
+			
+			progress_fhdl = open(progress_fpath, 'r+') # read and write data
+			progress_fhdl.seek(progress_record_start_pos, 0)
+			
+			if n_lines_previously_processed != 0:
+				continuum_fhdls = tuple(spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'rb+') for fpath in continuum_fpaths)
+				stronglines_fhdls = tuple(spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'ab') for fpath in stronglines_fpaths)
+			else:
+				continuum_fhdls = tuple(spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'wb') for fpath in continuum_fpaths)
+				stronglines_fhdls = tuple(spectral_data_source_helper.utils.structured_array.StructuredArrayFile(fpath,'wb') for fpath in stronglines_fpaths)
+			
 			for n_strong_lines, n_weak_lines_in_continuum, strong_lines_chunks, pseudo_continuum_contributions in ds_holder.iter_lines_and_continuum_at_temp(
 				T = temperature_arr, 
 				continuum_bin_edges = continuum_bin_edges,
@@ -333,36 +385,49 @@ def action_calc_continuum(
 				chunk_size=chunk_size,
 				trans_files_slice=slice(None),
 				#trans_files_slice=slice(None,3),
+				skip_n_lines = n_lines_previously_processed,
 			):
+				if stop_loop:
+					break
 				
 				total_strong_lines += n_strong_lines
 				total_weak_lines_in_continuum += n_weak_lines_in_continuum
-				
-				dt_start_write = dt.datetime.now()
-				
-				#strong_lines_chunks = tuple(strong_lines_chunks)
-				
-				for slc, fhdl in zip(strong_lines_chunks, stronglines_fhdls):
-					fhdl.write(slc)
 				
 				# Have to do summation field-by-field as numpy does not know how to do it for structured arrays
 				for field_name in pseudo_continuums.dtype.fields:
 					pseudo_continuums[field_name] += pseudo_continuum_contributions[field_name]
 				
-				# Write out continuum data-so-far to file
-				for i, (pc_part, fhdl) in enumerate(zip(pseudo_continuums, continuum_fhdls)):
-					fhdl.write(pseudo_continuums[i])
-					fhdl.seek(0,0)
+				
+				dt_start_write = dt.datetime.now()
+				
+				try:
+					for slc, fhdl in zip(strong_lines_chunks, stronglines_fhdls):
+						fhdl.write(slc)
+					
+					# Write out continuum data-so-far to file
+					for i, (pc_part, fhdl) in enumerate(zip(pseudo_continuums, continuum_fhdls)):
+						fhdl.write(pseudo_continuums[i])
+						fhdl.seek(0,0)
+					
+					progress_fhdl.write(f'{total_strong_lines[0] + total_weak_lines_in_continuum[0]} ')
+					np.savetxt(progress_fhdl, total_strong_lines, fmt='%d ', delimiter='', newline='', header='', footer='')
+					np.savetxt(progress_fhdl, total_weak_lines_in_continuum, fmt='%d ', delimiter='', newline='', header='', footer='')
+					progress_fhdl.write('\n')
+					progress_fhdl.seek(progress_record_start_pos, 0)
+				except KeyboardInterrupt:
+					stop_loop = True
 				
 				dt_split = dt.datetime.now()
-				dt_elapsed_delta = dt_split - dt_start
-				dt_elapsed_str = f'{dt_elapsed_delta.days}D {dt_elapsed_delta.seconds//3600}H {(dt_elapsed_delta.seconds %3600)//60}M {dt_elapsed_delta.seconds%60}s'
+				
 				
 				if (dt_split - dt_split_2).total_seconds() > 1:
+					dt_elapsed_delta = dt_split - dt_start
+					dt_elapsed_str = f'{dt_elapsed_delta.days}D {dt_elapsed_delta.seconds//3600}H {(dt_elapsed_delta.seconds %3600)//60}M {dt_elapsed_delta.seconds%60}s'
 				
 					#print(f'{strong_lines_chunks[0]=}')
 				
 					dt_split_2 = dt.datetime.now()
+					print()
 					print(f'Writing data to files took {1000*(dt_split - dt_start_write).total_seconds()} ms.')
 					
 					print('     Temperature | num. strong lines | num. weak lines | total strong lines | total weak lines')
@@ -370,14 +435,21 @@ def action_calc_continuum(
 						print(f'     {temp:11.2f} | {n_sl:17d} | {n_wl:16d} | {t_sl:18d} | {t_wl:16d}')
 					
 					print(f'Elapsed time: {dt_elapsed_str}')
-				
-				
 		
+		except KeyboardInterrupt:
+			stop_loop = True
 		finally:
+			if stop_loop:
+				print('\n\nCalculation Interrupted')
+			else:
+				print('\n\nCalculation Ended')
+			
+			progress_fhdl.close()
 			for fhdl in continuum_fhdls:
 				fhdl.close()
 			for fhdl in stronglines_fhdls:
 				fhdl.close()
+			
 
 
 def action_read_continuum(
@@ -657,6 +729,7 @@ if __name__=='__main__':
 	calc_continuum_parser.add_argument('-n', '--continuum_n_bins', type=int, help='Number of bins in the continuum', default=1_000)
 	calc_continuum_parser.add_argument('-s', '--continuum_bin_spacing', type=str, choices=('lin', 'log'), help='Spacing of continuum bins', default='lin')
 	calc_continuum_parser.add_argument('-c', '--chunk_size', type=int, help='Chunk size to use during calculations', default=CHUNK_SIZE)
+	calc_continuum_parser.add_argument('-R', '--force_restart', action='store_true', help='Force restart the calculation, do not continue from last position.', default=False)
 	
 	read_continuum_parser = subparsers.add_parser('read_continuum', help='read saved continuum data files')
 	read_continuum_parser.set_defaults(func = action_read_continuum)

spectral_data_source_helper/exomol/exomol_dataset_holder.py
@@ -14,6 +14,8 @@ from spectral_data_source_helper.utils import fetch
 from spectral_data_source_helper.utils import read
 from spectral_data_source_helper.utils import structured_array
 
+from ..progress_tracker.chunk import ChunkProgressTracker
+
 from .cfg.const import (
 	PKG_CACHE,
 	T_ref,
@@ -29,7 +31,7 @@ from . import qn_set_manager
 from . import broad_file_manager
 from .c_compat import ConvertTransToBin32#, run_main #convert_trans_to_bin32
 
-#from spectral_data_source_helper.cfg.log import progress_lgr
+from spectral_data_source_helper.cfg.log import progress_lgr
 from spectral_data_source_helper.cfg.log import pkg_logger as _lgr
 
 from .datatypes import (
@@ -692,13 +694,15 @@ class ExomolDatasetHolder:
 				best_fpath = fetch.file_from_cache(f'https://www.{api_trans_url}',cache=cache, return_fpath=True)
 			
 			yield best_fpath
-	
+
+
 	def iter_transitions(
 			self,
 			chunk_size : int = 1_000_000,
 			trans_files_slice : slice = slice(None),
 			trans_fpaths : None | Iterable[Path] = None, # If present iterate over these files, otherwise iterate over all of them
 			ensure_no_iterative_conversion : bool = False,
+			skip_n_transitions : int = 0, # Skip this number of transitions, used when continuing a previous iteration
 	) -> Generator[np.ndarray]:
 		
 		dt_start = dt.datetime.now()
@@ -720,13 +724,28 @@ class ExomolDatasetHolder:
 			trans_fpaths_iter = trans_fpaths
 		
 		
+		progress_tracker = ChunkProgressTracker(
+			lambda x: progress_lgr.info(str(x), stacklevel=4), 
+			rate_limit_timeout = 0.5, 
+			chunk_element_name='Record'
+		)
+		
 		for fpath, chunk in read.files_via_structured_array_chunk(
 				trans_fpaths_iter,
 				dtype=self.trans_dtype,
 				delim=None,
 				chunk_size=chunk_size,
 				yield_fpath = True,
+				progress_tracker = progress_tracker,
 		):
+			if skip_n_transitions > chunk.size:
+				skip_n_transitions -= chunk.size
+				continue
+			elif skip_n_transitions > 0:
+				chunk = chunk[skip_n_transitions:]
+				skip_n_transitions = 0
+				progress_tracker.reset()
+			
 			#_lgr.info(f'{fpath=} {len(chunk)=} {chunk[0]=}')
 			if self.get_transition_file_path_fmt_and_compression_str(fpath).startswith('.bin32'):
 				chunk['einstein_A'] /= TRANS_STR_FLOAT32_FACTOR
@@ -794,10 +813,11 @@ class ExomolDatasetHolder:
 						_lgr.info(f'Deleting downloaded file "{downloaded_trans_fpath.name}"')
 						downloaded_trans_fpath.unlink()
 			
-			if self.iterative_conversion_opts.delete_final_files_after_use:
-				_lgr.info(f'Deleting iteratively final converted file "{next_trans_fpaths[:-1].name}"')
-				if next_trans_fpaths[:-1].exists():
-					next_trans_fpaths[:-1].unlink()
+			if self.iterative_conversion_opts.delete_final_files_after_use and len(next_trans_fpaths) > 0:
+				final_fpath = next_trans_fpaths[-1]
+				_lgr.info(f'Deleting iteratively final converted file "{final_fpath.name}"')
+				if final_fpath.exists():
+					final_fpath.unlink()
 		
 	
 	
@@ -1096,6 +1116,7 @@ class ExomolDatasetHolder:
 			self,
 			chunk_size : int = 1_000_000,
 			trans_files_slice : slice = slice(None),
+			skip_n_transition_states : int = 0, # Skip this number of transition states, used when continuing a previous iteration
 	) -> Generator[np.ndarray]:
 		dt_start = dt.datetime.now()
 		_lgr.info(f'Starting to get transition state information at {dt_start}')
@@ -1110,7 +1131,7 @@ class ExomolDatasetHolder:
 		calc_wavenumber_flag = self.trans_n_cols < 4
 		
 		
-		for i, trans_chunk in enumerate(self.iter_transitions(chunk_size=chunk_size, trans_files_slice=trans_files_slice)):
+		for i, trans_chunk in enumerate(self.iter_transitions(chunk_size=chunk_size, trans_files_slice=trans_files_slice, skip_n_transitions = skip_n_transition_states)):
 			chunk_slice = slice(None, trans_chunk.size)
 			_lgr.debug(f'{trans_chunk.size=} {chunk_slice=} {chunk_size=}')
 			
@@ -1184,6 +1205,7 @@ class ExomolDatasetHolder:
 			self,
 			chunk_size : int = 1_000_000,
 			trans_files_slice : slice = slice(None),
+			skip_n_lines : int = 0, # Skip this number of lines, used when continuing a previous iteration
 	):		
 		line_data_chunk = np.empty((chunk_size,), dtype=self.line_data_dtype)
 		
@@ -1199,7 +1221,7 @@ class ExomolDatasetHolder:
 		broad_array_gas_slices, broad_array, broad_comp_mask, broad_values = self.broadening_data
 		
 		
-		for trans_states_chunk in self.iter_transition_states(chunk_size=chunk_size, trans_files_slice=trans_files_slice):
+		for trans_states_chunk in self.iter_transition_states(chunk_size=chunk_size, trans_files_slice=trans_files_slice, skip_n_transition_states=skip_n_lines):
 			chunk_slice = slice(None,len(trans_states_chunk))
 			line_data_chunk_part = line_data_chunk[chunk_slice]
 			
@@ -1291,6 +1313,7 @@ class ExomolDatasetHolder:
 					line_data_chunk[broad_source_name]
 				)
 			
+			
 			yield line_data_chunk_part
 			
 		
@@ -1302,16 +1325,21 @@ class ExomolDatasetHolder:
 		return np.interp(T, self.partition_function['T'], self.partition_function['Q'])
 	
 	
-	def get_line_and_continuum_fnames_at_temp(
+	def get_line_and_continuum_fpaths_at_temp(
 			self,
 			T_arr : np.ndarray,
 			temp_fmt : str = 'T{}',
-	) -> tuple[tuple[str,...],tuple[str,...],tuple[str,...]]:
-		contbins_fnames = tuple(self.datafile_prefix + '_' + temp_fmt.format(T) +'.contbins' for T in T_arr)
-		continuum_fnames = tuple(self.datafile_prefix + '_' + temp_fmt.format(T) +'.continuum' for T in T_arr)
-		stronglines_fnames = tuple(self.datafile_prefix + '_' + temp_fmt.format(T) +'.stronglines' for T in T_arr)
+			dir : Path = Path('./'),
+	) -> tuple[Path, tuple[Path,...],tuple[Path,...],tuple[Path,...]]:
+		temp_datafile_stems = tuple(self.datafile_prefix + '_' + temp_fmt.format(T) for T in T_arr)
 		
-		return (contbins_fnames, continuum_fnames, stronglines_fnames)
+		progress_fname = dir / (self.datafile_prefix+'.cont_progress')
+		
+		contbins_fnames = tuple(dir / (s+'.contbins') for s in temp_datafile_stems)
+		continuum_fnames = tuple(dir / (s+'.continuum') for s in temp_datafile_stems)
+		stronglines_fnames = tuple(dir / (s+'.stronglines') for s in temp_datafile_stems)
+		
+		return (progress_fname, contbins_fnames, continuum_fnames, stronglines_fnames)
 	
 	@property
 	def pseudo_continuum_contribution_dtype_list(self) -> list[tuple[str,Any],...]:
@@ -1350,6 +1378,7 @@ class ExomolDatasetHolder:
 			continuum_line_intensity_cutoff : float = 1E-24,
 			chunk_size : int = 1_000_000,
 			trans_files_slice : slice = slice(None),
+			skip_n_lines : int = 0, # Skip this number of lines, used when continuing a previous iteration.
 	) -> Generator[tuple[np.ndarray, np.ndarray, tuple[np.ndarray], np.ndarray]]:
 		
 		n_temps = T.size
@@ -1395,11 +1424,10 @@ class ExomolDatasetHolder:
 		ldc_struct_names = [x[1] for x in pseudo_continuum_var_name_pair_tuple]
 		
 		
-		for line_data_chunk in self.iter_line_data(chunk_size=chunk_size, trans_files_slice=trans_files_slice):
-			
+		for line_data_chunk in self.iter_line_data(chunk_size=chunk_size, trans_files_slice=trans_files_slice, skip_n_lines=skip_n_lines):
 			chunk_slice = slice(None, line_data_chunk.size)
-			strong_line_mask_part = strong_line_mask[:, chunk_slice]
 			
+			strong_line_mask_part = strong_line_mask[:, chunk_slice]
 			stimulated_emission_ratio_part = stimulated_emission_ratio[:,chunk_slice]
 			boltz_pop_ratio_part = boltz_pop_ratio[:,chunk_slice]
 			line_strengths_at_temp_part = line_strengths_at_temp[:, chunk_slice]
@@ -1420,27 +1448,8 @@ class ExomolDatasetHolder:
 				copy = False
 			)
 			assert ldc_view.base is not None, "Must be able to build a view of `line_data_chunk`"
-			"""
-			
-			# NOTE: We need to have any entries in `line_data_chunk` that have problematic values to have `spec_line_intensity` set to zero
-			# by this point. That way erroneous values will not have any effect on the output.
-			
-			_lgr.info(f'{line_data_chunk.size=}')
-			#print(f'{np.count_nonzero(np.isnan(line_data_chunk['spec_line_intensity']))=}')
-			#print(f'{np.count_nonzero(line_data_chunk['wavenumber'] == 0)=}')
-			
-			# set continuum contribution for this chunk to zero
 			
 			
-			#wavenumber_gt_zero_mask[chunk_slice] = line_data_chunk['wavenumber'] > 0
-			
-			
-			#stimulated_emission_ratio[:,chunk_slice][:, wavenumber_gt_zero_mask[chunk_slice]] = (
-			#	one_minus_exp_c2_nu(T_ref,line_data_chunk['wavenumber'][wavenumber_gt_zero_mask[chunk_slice]]) 
-			#	/ one_minus_exp_c2_nu(T, line_data_chunk["spec_line_factor_one_minus_exp_wavenumber"][wavenumber_gt_zero_mask[chunk_slice]])
-			#)
-			
-			"""
 			spectral_data_source_helper.calc.numba.spec.stimulated_emission_v(
 				line_data_chunk['wavenumber'],
 				T,
@@ -1517,6 +1526,7 @@ class ExomolDatasetHolder:
 			_lgr.debug(f'{n_weak_lines_in_continuum=} {n_weak_lines_outside_continuum=}')
 			
 			
+			# This is quite slow
 			spectral_data_source_helper.calc.numba.spec.accumulate_pseudocontinuum(
 				weak_line_mask_part,
 				bin_indices_part,
@@ -1528,10 +1538,11 @@ class ExomolDatasetHolder:
 			
 			
 			
+			
 			yield (
 				n_strong_lines, 
 				n_weak_lines_in_continuum, 
-				(line_data_chunk[strong_line_mask_part[j]] for j in range(n_temps)), 
+				[line_data_chunk[strong_line_mask_part[j]] for j in range(n_temps)], 
 				pseudo_continuum_contribution
 			)
 			_lgr.debug('strong lines and continuum data outputted')

spectral_data_source_helper/utils/structured_array.py
@@ -23,10 +23,11 @@ module_progress_sink : ModuleVar = ModuleVar(
 class StructuredArrayFile:
 	class_writable_modes : tuple[str,...] = ('wb', 'rb+', 'ab')
 	class_readable_modes : tuple[str,...] = ('rb', 'rb+')
+	class_updateable_modes : tuple[str,...] = ('ab', 'rb+')
 	
 	def __init__(
 			self, 
-			fpath, mode : None | Literal['wb', 'rb'] = None,
+			fpath, mode : None | Literal['wb', 'rb', 'ab', 'rb+'] = None,
 			reader : None | BinaryReader = None,
 	):
 		self.fpath = fpath
@@ -50,16 +51,15 @@ class StructuredArrayFile:
 	def __del__(self):
 		self.close()
 	
-	def open(self, mode : Literal['wb', 'rb']) -> Self:
+	def open(self, mode : Literal['wb', 'rb', 'ab', 'rb+']) -> Self:
 		
-		assert mode in ('wb', 'rb'), "`mode` must be one of ('wb', 'rb')"
+		assert mode in ('wb', 'rb', 'ab', 'rb+'), "`mode` must be one of ('wb', 'rb', 'ab', 'rb+')"
 		
 		if self.fhdl is not None:
 			assert self.mode == mode, "Cannot reopen file with a different mode before closing it"
 			return self
 		
 		self.mode = mode
-		
 
 		self.fhdl = open(self.fpath, self.mode)
 		
@@ -74,6 +74,14 @@ class StructuredArrayFile:
 		self.header_byte_end = 0
 		self.header_written = False
 		self.arr_dtype = None
+		
+		if self.mode in self.class_updateable_modes:
+			if self.mode == 'rb+':
+				self.arr_dtype = self.read_dtype()
+				if self.arr_dtype is not None:
+					self.header_written = True
+			else:
+				self.header_written = True # assume header is written
 		
 		return self
 	
@@ -117,6 +125,10 @@ class StructuredArrayFile:
 		#print(f'reading dtype {self.fhdl.name=}', flush=True)
 		# Read 4 bytes at a time until string ends with null character
 		hdr_part = self.read_bytes(4)
+		if len(hdr_part) == 0:
+			self.header_byte_end = 0
+			return None
+		
 		while hdr_part[-1] != NULL_BYTE and len(hdr_part) <= HDR_MAX_SIZE:
 			hdr_part += self.read_bytes(4)
 		
@@ -129,11 +141,14 @@ class StructuredArrayFile:
 		return hdr_part.decode(encoding)
 	
 	def read_dtype(self, encoding : str = 'ascii'):
-		return spectral_data_source_helper.utils.dtype.from_string(self.read_header(encoding=encoding))
+		hdr = self.read_header(encoding=encoding)
+		return spectral_data_source_helper.utils.dtype.from_string(hdr) if hdr is not None else None
 	
 	def read(self, count : int = -1) -> np.ndarray:	
 		if self.arr_dtype is None:
 			self.arr_dtype = self.read_dtype()
+
+		#print(f'{self.arr_dtype=}')
 
 		result = np.frombuffer(
 			self.read_bytes(count*self.arr_dtype.itemsize), 